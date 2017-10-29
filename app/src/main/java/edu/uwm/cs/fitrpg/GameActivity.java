c8b24607a41ca279d2323c276b47585175b4f1a4
SS Fink <the.jabber.jockey@gmail.com>
2017-10-28 20:11:32 -0500

    Rejected commit: Default

---
+++ app/src/main/java/edu/uwm/cs/fitrpg/GameActivity.java
@@ -0,0 +1,77 @@
+package edu.uwm.cs.fitrpg;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import edu.uwm.cs.fitrpg.graphics.AnimatedSprite;
+import edu.uwm.cs.fitrpg.graphics.Particle;
+import edu.uwm.cs.fitrpg.graphics.Scene;
+import edu.uwm.cs.fitrpg.graphics.GameView;
+
+public class GameActivity extends Activity {
+    private GameView gv;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        gv = new GameView(this);
+
+        setUpScene(gv);
+
+        setContentView(gv);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+    }
+
+
+    private void setUpScene(GameView gv)
+    {
+        gv.getScene().setBackground(Color.parseColor("#4B0082"));
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.test);
+        bm = Bitmap.createScaledBitmap(bm, 128, 32,false);
+        gv.getScene().spawnNewSprite(bm,32,32,4,true,0,0);
+        bm = BitmapFactory.decodeResource(getResources(), R.drawable.jki);
+        bm = Bitmap.createScaledBitmap(bm, 128, 32,false);
+        gv.getScene().spawnNewSprite(bm,32,32,4,true,250,250);
+        gv.getScene().getSpriteByIndex(1).setScale(4);
+        gv.getScene().getSpriteByIndex(1).translate(200, 200, 1);
+        gv.getScene().writeText("Hello World!", Color.WHITE, 500, 500, 64, true);
+        //gv.getScene().removeSprite(gv.getScene().getSpriteByIndex(0));
+
+
+
+    }
+
+
+}
+
