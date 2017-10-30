322ea0e4262f105a625cdb64d04598ff5cae0b02
Jason Klein <kleinjd@uwm.edu>
2017-10-30 17:43:57 -0500

    Update DatabaseHelper.java

--- app/src/main/java/edu/uwm/cs/fitrpg/DatabaseHelper.java
+++ app/src/main/java/edu/uwm/cs/fitrpg/DatabaseHelper.java
@@ -20,7 +20,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table fr_act (act_id INTEGER(2) PRIMARY KEY, act_dsc VARCHAR(50)");
+        db.execSQL("create table fr_act (act_id INTEGER(2) PRIMARY KEY, act_dsc VARCHAR(50))");
         db.execSQL("create table fr_char (usr_id INTEGER(3) PRIMARY KEY, usr_nam VARCHAR(13), nd_pos INTEGER(2), a_str INTEGER(2), a_spd INTEGER(2), a_sta INTEGER(2), a_dex INTEGER(2), a_end INTEGER(2))");
         db.execSQL("create table fr_hst (act_num INTEGER(4) PRIMARY KEY, act_id INTEGER(2) NOT NULL, usr_id INTEGER(3) NOT NULL, s_tme TEXT, e_tme TEXT, dist FLOAT, reps INTEGER(3))");
         db.execSQL("create table fr_map (usr_id INTEGER(3) PRIMARY KEY, map_id INTEGER(2) PRIMARY KEY, nd_id INTEGER(2) PRIMARY KEY, nd_cmp INTEGER(1) DEFAULT 0)");
