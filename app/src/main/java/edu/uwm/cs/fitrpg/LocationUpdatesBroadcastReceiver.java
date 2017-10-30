fb4c5874649bf6de7b09982e913a334a2fb2b59b
Paul Burrie <burriep@gmail.com>
2017-10-29 19:13:51 -0500

    More progress on fitness activity tracking

--- app/src/main/java/edu/uwm/cs/fitrpg/LocationUpdatesBroadcastReceiver.java
+++ app/src/main/java/edu/uwm/cs/fitrpg/LocationUpdatesBroadcastReceiver.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes to the sample code from Google have been made to customize it to the needs of this app.
  */
 
 package edu.uwm.cs.fitrpg;
@@ -20,31 +22,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.util.Log;
-
 import com.google.android.gms.location.LocationResult;
 
 import java.util.List;
 
 /**
  * Receiver for handling location updates.
- *
- * For apps targeting API level O
- * {@link android.app.PendingIntent#getBroadcast(Context, int, Intent, int)} should be used when
- * requesting location updates. Due to limits on background services,
- * {@link android.app.PendingIntent#getService(Context, int, Intent, int)} should not be used.
- *
- *  Note: Apps running on "O" devices (regardless of targetSdkVersion) may receive updates
- *  less frequently than the interval specified in the
- *  {@link com.google.android.gms.location.LocationRequest} when the app is no longer in the
- *  foreground.
  */
 public class LocationUpdatesBroadcastReceiver extends BroadcastReceiver {
-    private static final String TAG = "LUBroadcastReceiver";
-
-    static final String ACTION_PROCESS_UPDATES =
-            "com.google.android.gms.location.sample.locationupdatespendingintent.action" +
-                    ".PROCESS_UPDATES";
+    static final String ACTION_PROCESS_UPDATES = "edu.uwm.cs.fitrpg.action.PROCESS_UPDATES";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -55,8 +41,6 @@ public class LocationUpdatesBroadcastReceiver extends BroadcastReceiver {
                 if (result != null) {
                     List<Location> locations = result.getLocations();
                     Utils.setLocationUpdatesResult(context, locations);
-                    Utils.sendNotification(context, Utils.getLocationResultTitle(context, locations));
-                    Log.i(TAG, Utils.getLocationUpdatesResult(context));
                 }
             }
         }
--- app/src/main/java/edu/uwm/cs/fitrpg/Utils.java
+++ app/src/main/java/edu/uwm/cs/fitrpg/Utils.java
@@ -12,33 +12,23 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes to the sample code from Google have been made to customize it to the needs of this app.
  */
 
 package edu.uwm.cs.fitrpg;
 
-
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.location.Location;
 import android.preference.PreferenceManager;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
-
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.List;
 
-/**
- * Utility methods used in this sample.
- */
 class Utils {
 
     final static String KEY_LOCATION_UPDATES_REQUESTED = "location-updates-requested";
     final static String KEY_LOCATION_UPDATES_RESULT = "location-update-result";
+
+    public static FitnessActivity currentActivity;
 
     static void setRequestingLocationUpdates(Context context, boolean value) {
         PreferenceManager.getDefaultSharedPreferences(context)
@@ -53,91 +43,21 @@ class Utils {
     }
 
     /**
-     * Posts a notification in the notification bar when a transition is detected.
-     * If the user clicks the notification, control goes to the MainActivity.
+     * Called when new Locations are received.
+     * @param context
+     * @param locations
      */
-    static void sendNotification(Context context, String notificationDetails) {
-        // Create an explicit content Intent that starts the main Activity.
-        Intent notificationIntent = new Intent(context, MainActivity.class);
-
-        notificationIntent.putExtra("from_notification", true);
-
-        // Construct a task stack.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-
-        // Add the main Activity to the task stack as the parent.
-        stackBuilder.addParentStack(MainActivity.class);
-
-        // Push the content Intent onto the stack.
-        stackBuilder.addNextIntent(notificationIntent);
-
-        // Get a PendingIntent containing the entire back stack.
-        PendingIntent notificationPendingIntent =
-                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // Get a notification builder that's compatible with platform versions >= 4
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-
-        // Define the notification settings.
-        builder.setSmallIcon(R.mipmap.ic_launcher)
-                // In a real app, you may want to use a library like Volley
-                // to decode the Bitmap.
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
-                        R.mipmap.ic_launcher))
-                .setColor(Color.RED)
-                .setContentTitle("Location update")
-                .setContentText(notificationDetails)
-                .setContentIntent(notificationPendingIntent);
-
-        // Dismiss notification once the user touches it.
-        builder.setAutoCancel(true);
-
-        // Get an instance of the Notification manager
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Issue the notification
-        mNotificationManager.notify(0, builder.build());
-    }
-
-
-    /**
-     * Returns the title for reporting about a list of {@link Location} objects.
-     *
-     * @param context The {@link Context}.
-     */
-    static String getLocationResultTitle(Context context, List<Location> locations) {
-        String numLocationsReported = context.getResources().getQuantityString(
-                R.plurals.num_locations_reported, locations.size(), locations.size());
-        return numLocationsReported + ": " + DateFormat.getDateTimeInstance().format(new Date());
-    }
-
-    /**
-     * Returns te text for reporting about a list of  {@link Location} objects.
-     *
-     * @param locations List of {@link Location}s.
-     */
-    private static String getLocationResultText(Context context, List<Location> locations) {
-        if (locations.isEmpty()) {
-            return context.getString(R.string.unknown_location);
-        }
-        StringBuilder sb = new StringBuilder();
-        for (Location location : locations) {
-            sb.append("(");
-            sb.append(location.getLatitude());
-            sb.append(", ");
-            sb.append(location.getLongitude());
-            sb.append(")");
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-
     static void setLocationUpdatesResult(Context context, List<Location> locations) {
+        // TODO: store data in db
+        FitnessActivity activity = currentActivity;
+        if (activity != null) {
+            activity.addLocations(locations);
+        }
+        String updatedLocationResult = "";
+
         PreferenceManager.getDefaultSharedPreferences(context)
                 .edit()
-                .putString(KEY_LOCATION_UPDATES_RESULT, getLocationResultTitle(context, locations)
-                        + "\n" + getLocationResultText(context, locations))
+                .putString(KEY_LOCATION_UPDATES_RESULT, updatedLocationResult)
                 .apply();
     }
 
