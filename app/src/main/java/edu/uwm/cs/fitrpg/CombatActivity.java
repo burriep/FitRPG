9b3d3e9a4762e7c5e5081641b0d914ffc9242c7f
Patrick Seaton Desktop <ronike21@gmail.com>
2017-10-31 20:32:30 -0500

    Basic Combat Engine is complete, including speed.

--- app/src/main/java/edu/uwm/cs/fitrpg/CombatActivity.java
+++ app/src/main/java/edu/uwm/cs/fitrpg/CombatActivity.java
@@ -1,11 +1,16 @@
 package edu.uwm.cs.fitrpg;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+/**
+ * Created by Patrick Seaton on 10/30/2017.
+ */
 
 public class CombatActivity extends AppCompatActivity {
 
@@ -18,6 +23,10 @@ public class CombatActivity extends AppCompatActivity {
     private ImageView enemyHealthBar;
     private TextView playerHealthLabel;
     private TextView enemyHealthLabel;
+    private TextView combatLog;
+    private int combatLogLines;
+
+    private long lastAttackTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,21 +75,100 @@ public class CombatActivity extends AppCompatActivity {
         textHolder = (TextView) findViewById((R.id.CombatEnemySpeedAmount));
         textHolder.setText(Integer.toString(enemyUnit.GetSpeed())+ " SPD");
 
-        defaultHealthBarWidth = findViewById(R.id.CombatEnemyHealthBarEmpty).getWidth();
         playerHealthBar = (ImageView)findViewById(R.id.CombatPlayerHealthBar);
         enemyHealthBar = (ImageView)findViewById(R.id.CombatEnemyHealthBar);
+        defaultHealthBarWidth = playerHealthBar.getLayoutParams().width;
+        combatLog = (TextView) findViewById(R.id.CombatLog);
+        combatLogLines = 0;
 
+        final Handler enemyAttackHandler = new Handler();
+        enemyAttackHandler.postDelayed(new Runnable(){
+            public void run(){
+                RunEnemyAttack();
+                if(playerUnit.GetCurrentHP() > 0)
+                {
+                    enemyAttackHandler.postDelayed(this, (int)(1/Math.log((double)(enemyUnit.GetSpeed() + 1)) * 1000));
+                }
+            }
+        }, (int)(1/Math.log((double)(enemyUnit.GetSpeed() + 1)) * 1000));
+        lastAttackTime = 0;
     }
 
     public void RunCombatRound(View view)
     {
-        if(playerUnit.GetCurrentHP() > 0) {
-            playerUnit.Attack(enemyUnit);
-        }
-        if(enemyUnit.GetCurrentHP() > 0) {
-            enemyUnit.Attack(playerUnit);
+        RunPlayerAttack();
+    }
+
+    public void RunEnemyAttack()
+    {
+        if(enemyUnit.GetCurrentHP() > 0 && playerUnit.GetCurrentHP() > 0) {
+            int damageAmount = enemyUnit.Attack(playerUnit);
+            String logAddition = "\nEnemy Attacked! ";
+            if(damageAmount > 0)
+            {
+                logAddition += "Dealt " + Integer.toString(damageAmount) + " damage! ";
+                if(playerUnit.GetCurrentHP() <= 0)
+                {
+                    logAddition += "Player was KO'd!";
+                }
+            }
+            else
+            {
+                logAddition += "Enemy Missed! ";
+            }
+            combatLogLines++;
+            String initialText = (String)combatLog.getText();
+            if(combatLogLines > 6)
+            {
+                initialText = (String)initialText.subSequence(initialText.indexOf("\n")+1, initialText.length());
+            }
+            combatLog.setText(initialText + logAddition);
         }
         playerHealthLabel.setText(Integer.toString(playerUnit.GetCurrentHP()) + "/" + Integer.toString(playerUnit.GetMaxHP()));
+        playerHealthBar.getLayoutParams().width = (int)(defaultHealthBarWidth * ((double)playerUnit.GetCurrentHP()/(double)playerUnit.GetMaxHP()));
+    }
+
+    public void RunPlayerAttack()
+    {
+        long currentTime = System.currentTimeMillis();
+        String logAddition = "\n";
+        if(currentTime - lastAttackTime > (1/Math.log((double)(playerUnit.GetSpeed() + 1)) * 500) && playerUnit.GetCurrentHP() > 0 && enemyUnit.GetCurrentHP() > 0) {
+            int damageAmount = playerUnit.Attack(enemyUnit);
+            logAddition += "Player Attacked! ";
+            if(damageAmount > 0)
+            {
+                logAddition += "Dealt " + Integer.toString(damageAmount) + " damage! ";
+                if(enemyUnit.GetCurrentHP() <= 0)
+                {
+                    logAddition += "Enemy was KO'd!";
+                }
+            }
+            else
+            {
+                logAddition += "Player Missed! ";
+            }
+            lastAttackTime = currentTime;
+        }
+        else if(currentTime - lastAttackTime <= (1/Math.log((double)(playerUnit.GetSpeed() + 1)) * 500))
+        {
+            logAddition += "Player Attack is still on Cooldown!";
+        }
+        else if(playerUnit.GetCurrentHP() > 0)
+        {
+            logAddition += "Player is KO'd and unable to attack!";
+        }
+        else
+        {
+            logAddition += "Enemy is already KO'd!";
+        }
+        combatLogLines++;
+        String initialText = (String)combatLog.getText();
+        if(combatLogLines > 6)
+        {
+            initialText = (String)initialText.subSequence(initialText.indexOf("\n")+1, initialText.length());
+        }
+        combatLog.setText(initialText + logAddition);
         enemyHealthLabel.setText(Integer.toString(enemyUnit.GetCurrentHP()) + "/" + Integer.toString(enemyUnit.GetMaxHP()));
+        enemyHealthBar.getLayoutParams().width = (int)(defaultHealthBarWidth * ((double)enemyUnit.GetCurrentHP()/(double)enemyUnit.GetMaxHP()));
     }
 }
--- app/src/main/java/edu/uwm/cs/fitrpg/CombatUnit.java
+++ app/src/main/java/edu/uwm/cs/fitrpg/CombatUnit.java
@@ -1,10 +1,17 @@
 package edu.uwm.cs.fitrpg;
 
+import java.util.Random;
+
 /**
  * Created by Patrick Seaton on 10/27/2017.
  */
 
 public class CombatUnit {
+
+    private double staminaToHpConversion = 3;   //PS How much extra HP one point of Stamina is worth (I.E if Base is 20, and this is 3, and Stamina is 10, max HP is 50)
+    private int baseHitChance = 80;             //PS If at even dexterity, how much chance an attack has to hit out of 100
+    private int lowestHitChance = 33;           //PS The lowest the hit chance can possibly be out of 100
+    private int strengthWeightVsEndurance = 2;  //PS The multiplier for strength before the target's endurance is taken into account
 
     private int stamina;
     private int strength;
@@ -13,13 +20,18 @@ public class CombatUnit {
     private int speed;
 
     private int currentHP;
-    private double staminaToHpConversion = 3;
+    private Random rand;
 
     public int GetStamina(){return stamina;}
+    public void SetStamina(int val){stamina = val;}
     public int GetStrength(){return strength;}
+    public void SetStrength(int val){strength = val;}
     public int GetEndurance(){return endurance;}
+    public void SetEndurance(int val){endurance = val;}
     public int GetDexterity(){return dexterity;}
+    public void SetDexterity(int val){dexterity = val;}
     public int GetSpeed(){return speed;}
+    public void SetSpeed(int val){speed = val;}
 
     public int GetCurrentHP(){return currentHP;}
     public int GetMaxHP(){return (int)(stamina * staminaToHpConversion);}
@@ -33,6 +45,7 @@ public class CombatUnit {
         speed = initSpeed;
 
         currentHP = (int)(stamina * staminaToHpConversion);
+        rand = new Random();
     }
 
     public void Init(int initStamina, int initStrength, int initEndurance, int initDexterity, int initSpeed)
@@ -46,9 +59,24 @@ public class CombatUnit {
         currentHP = (int)(stamina * staminaToHpConversion);
     }
 
-    public void Attack(CombatUnit target)
+    public int Attack(CombatUnit target)
     {
-        target.Damage((strength * 2) - target.endurance);
+        int damageAmount = (strength * strengthWeightVsEndurance) - target.endurance;
+        if(damageAmount < 1)
+        {
+            damageAmount = 1;
+        }
+        int hitChance = baseHitChance - (target.dexterity - dexterity);
+        if(hitChance < lowestHitChance)
+        {
+            hitChance = lowestHitChance;
+        }
+        if(rand.nextInt(100) > hitChance)
+        {
+            damageAmount = 0;
+        }
+        target.Damage(damageAmount);
+        return damageAmount;
     }
 
     public void Damage(int amount)
