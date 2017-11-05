e2f78c828e17f95b7fe3a49bd4103ae24fc50040
SS Fink <the.jabber.jockey@gmail.com>
2017-11-04 20:45:32 -0500

    changed the sprites to be using the player spritesheet

--- app/src/main/java/edu/uwm/cs/fitrpg/view/GameActivity.java
+++ app/src/main/java/edu/uwm/cs/fitrpg/view/GameActivity.java
@@ -1,4 +1,4 @@
-package edu.uwm.cs.fitrpg;
+package edu.uwm.cs.fitrpg.view;
 
 import android.app.Activity;
 import android.content.Context;
@@ -8,6 +8,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v4.view.MotionEventCompat;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -15,6 +18,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import edu.uwm.cs.fitrpg.R;
 import edu.uwm.cs.fitrpg.graphics.AnimatedSprite;
 import edu.uwm.cs.fitrpg.graphics.Particle;
 import edu.uwm.cs.fitrpg.graphics.Scene;
@@ -22,10 +26,6 @@ import edu.uwm.cs.fitrpg.graphics.GameView;
 
 public class GameActivity extends Activity {
     private GameView gv;
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +40,6 @@ public class GameActivity extends Activity {
 
         setContentView(gv);
     }
-
     @Override
     protected void onPause()
     {
@@ -54,19 +53,24 @@ public class GameActivity extends Activity {
     }
 
 
+
+
+
     private void setUpScene(GameView gv)
     {
         gv.getScene().setBackground(Color.parseColor("#4B0082"));
-        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.test);
-        bm = Bitmap.createScaledBitmap(bm, 128, 32,false);
-        gv.getScene().spawnNewSprite(bm,32,32,4,true,0,0);
-        bm = BitmapFactory.decodeResource(getResources(), R.drawable.jki);
-        bm = Bitmap.createScaledBitmap(bm, 128, 32,false);
-        gv.getScene().spawnNewSprite(bm,32,32,4,true,250,250);
-        gv.getScene().getSpriteByIndex(1).setScale(4);
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.player_ss_test);
+        bm = Bitmap.createScaledBitmap(bm, 576, 384,false);
+        gv.getScene().spawnNewSprite(bm,64,64, 9,true,0,0);
+        gv.getScene().spawnNewSprite(bm,64,64,9,true,250,250);
+        gv.getScene().getSpriteByIndex(1).setScale(8);
+        gv.getScene().getSpriteByIndex(0).setScale(8);
+        gv.getScene().getSpriteByIndex(1).setSpriteSheetRow(2);
+        gv.getScene().getSpriteByIndex(0).setSpriteSheetRow(5);
+        gv.getScene().getSpriteByIndex(0).setNumFrames(6);
         gv.getScene().getSpriteByIndex(1).translate(200, 200, 1);
-        gv.getScene().writeText("Hello World!", Color.WHITE, 500, 500, 64, true);
-        //gv.getScene().removeSprite(gv.getScene().getSpriteByIndex(0));
+        gv.getScene().writeText("Click Me!", Color.WHITE, 500, 500, 64, true);
+       //gv.getScene().removeSprite(gv.getScene().getSpriteByIndex(0));
 
 
 
