6b86a1921918dd957f4f5689e2f51889e11d57d4
Paul Burrie <burriep@gmail.com>
2017-11-05 18:30:36 -0600

    Refactor FitnessActivity to prep for persistant storage
    Also fixed some bugs I noticed while refactoring.

--- app/src/main/java/edu/uwm/cs/fitrpg/model/FitnessActivity.java
+++ app/src/main/java/edu/uwm/cs/fitrpg/model/FitnessActivity.java
@@ -2,100 +2,109 @@ package edu.uwm.cs.fitrpg.model;
 
 import android.location.Location;
 
+import java.io.Serializable;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
-public class FitnessActivity {
-    private List<FitnessActivitySegment> segments = new LinkedList<>();
-    private double cachedDistance;
-    private long cachedDuration;
-    private double cachedTopSpeed;
+public class FitnessActivity implements Serializable {
+    private static final int NS_IN_MS = 1000000;
+    private static final int MS_IN_SECOND = 1000;
+    private long startTime;
+    private long stopTime;
+    private double cachedDistance; // in meters
+    private long cachedDuration; // in ms
+    private double cachedTopSpeed; // in meters / s
+
+    private Location lastLocation;
+    private long segmentStartTime; // in ms
+    private double segmentDistance; // in meters
+    private double segmentTopSpeed; // in meters / s
 
     public void start() {
-        FitnessActivitySegment segment = new FitnessActivitySegment();
-        segments.add(segment);
-        segment.setStartTime(android.os.SystemClock.elapsedRealtimeNanos());
+        if (isTracking()) {
+            throw new IllegalStateException("Already started.");
+        }
+        segmentStartTime = android.os.SystemClock.elapsedRealtime();
+        if (startTime <= 0) {
+            startTime = segmentStartTime;
+        }
     }
 
     public void stop() {
-        if (segments.isEmpty()) {
-            throw new IllegalStateException();
+        if (!isTracking()) {
+            throw new IllegalStateException("Already stopped.");
         }
-        FitnessActivitySegment segment = segments.get(segments.size() - 1);
-        if (!segment.isStarted() || segment.isStopped()) {
-            throw new IllegalStateException();
-        }
-        segment.setStopTime(android.os.SystemClock.elapsedRealtimeNanos());
-        cachedDistance += segment.getDistance();
-        cachedDuration += segment.getDuration();
-        cachedTopSpeed = Math.max(cachedTopSpeed, segment.getTopSpeed());
+        stopTime = android.os.SystemClock.elapsedRealtime();
+        cachedDistance += segmentDistance;
+        cachedDuration += (stopTime - segmentStartTime);
+        cachedTopSpeed = Math.max(cachedTopSpeed, segmentTopSpeed);
+        // reset for next segment
+        lastLocation = null;
+        segmentStartTime = 0;
+        segmentDistance = 0;
+        segmentTopSpeed = 0;
     }
 
-    public boolean isStarted() {
-        if (!segments.isEmpty()) {
-            FitnessActivitySegment segment = segments.get(segments.size() - 1);
-            return segment.isStarted() && !segment.isStopped();
-        }
-        return false;
+    public boolean isTracking() {
+        return segmentStartTime > 0;
+    }
+
+    public boolean hasBeenStarted() {
+        return startTime > 0;
     }
 
     public void addLocation(Location l) {
-        if (segments.isEmpty()) {
+        if (!isTracking()) {
             throw new IllegalStateException();
         }
-        FitnessActivitySegment segment = segments.get(segments.size() - 1);
-        if (!segment.isStarted() || segment.isStopped()) {
-            throw new IllegalStateException();
+        // Location.getElapsedRealtimeNanos() is used because it is guaranteed to be monotonically non-decreasing, unlike Location.getTime()
+        long locationTimestamp = l.getElapsedRealtimeNanos() / NS_IN_MS;
+        // only consider location updates during the activity.
+        if (locationTimestamp >= startTime) {
+            if (lastLocation != null) {
+                long timeDifference = (l.getElapsedRealtimeNanos() - lastLocation.getElapsedRealtimeNanos()) / NS_IN_MS;
+                // only consider location updates which are came after the previous location update.
+                if (timeDifference >= 0) {
+                    // find distance between this location and the last location
+                    double latestDistance = l.distanceTo(lastLocation);
+                    segmentDistance += latestDistance;
+                    // avoid divide-by-zero
+                    if (timeDifference > 0) {
+                        // calculate the speed needed to travel that distance in that time, in meters per second
+                        double latestTopSpeed = (latestDistance * MS_IN_SECOND) / timeDifference;
+                        segmentTopSpeed = Math.max(segmentTopSpeed, latestTopSpeed);
+                    }
+                }
+            }
+            lastLocation = l;
         }
-        segment.addLocation(l);
     }
 
     public void addLocations(List<Location> locations) {
-        if (segments.isEmpty()) {
-            throw new IllegalStateException();
+        for (Location l : locations) {
+            addLocation(l);
         }
-        FitnessActivitySegment segment = segments.get(segments.size() - 1);
-        if (!segment.isStarted() || segment.isStopped()) {
-            throw new IllegalStateException();
-        }
-        segment.addLocations(locations);
     }
 
     public double getDistance() {
-        double d = cachedDistance;
-        if (!segments.isEmpty()) {
-            FitnessActivitySegment segment = segments.get(segments.size() - 1);
-            if (!segment.isStopped()) {
-                d += segment.getDistance();
-            }
-        }
-        return d;
+        return cachedDistance + segmentDistance;
     }
 
     public long getDuration() {
         long d = cachedDuration;
-        if (!segments.isEmpty()) {
-            FitnessActivitySegment segment = segments.get(segments.size() - 1);
-            if (!segment.isStopped()) {
-                d += (android.os.SystemClock.elapsedRealtimeNanos() - segment.getStartTime());
-            }
+        if (isTracking()) {
+            d += (android.os.SystemClock.elapsedRealtime() - segmentStartTime);
         }
         return d;
     }
 
     public double getAverageSpeed() {
         double dur = getDuration();
-        return (dur > 0) ? getDistance() / dur : 0;
+        return (dur > 0) ? (getDistance() * MS_IN_SECOND / dur) : 0;
     }
 
     public double getTopSpeed() {
-        double s = cachedTopSpeed;
-        if (!segments.isEmpty()) {
-            FitnessActivitySegment segment = segments.get(segments.size() - 1);
-            if (!segment.isStopped()) {
-                s = Math.max(s, segment.getTopSpeed());
-            }
-        }
-        return s;
+        return Math.max(cachedTopSpeed, segmentTopSpeed);
     }
 }
