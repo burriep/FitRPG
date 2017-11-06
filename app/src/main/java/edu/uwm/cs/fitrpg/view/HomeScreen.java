3d99fbe0925b9b9cc50f03f9606f2ff934d27ac5
Tyler Breckenridge <brecken4@uwm.edu>
2017-11-06 16:54:40 -0600

    more button!

--- app/src/main/java/edu/uwm/cs/fitrpg/view/HomeScreen.java
+++ app/src/main/java/edu/uwm/cs/fitrpg/view/HomeScreen.java
@@ -17,7 +17,7 @@ import edu.uwm.cs.fitrpg.fragments.GotoGameFragment;
 
 public class HomeScreen extends AppCompatActivity{
 
-    ImageButton ibGame, ibFitness;
+    ImageButton ibGame, ibFitness, ibGraphics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +35,7 @@ public class HomeScreen extends AppCompatActivity{
 
         ibGame = (ImageButton) findViewById(R.id.btn_go_to_game);
         ibFitness = (ImageButton) findViewById(R.id.btn_go_to_fitness);
+        ibGraphics = (ImageButton) findViewById(R.id.btn_go_to_graphics);
 
         ibGame.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +48,13 @@ public class HomeScreen extends AppCompatActivity{
             @Override
             public void onClick(View view) {
                 gotoFitness();
+            }
+        });
+
+        ibGraphics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoGraphics();
             }
         });
 
@@ -95,6 +103,11 @@ public class HomeScreen extends AppCompatActivity{
 
     public void gotoFitness() {
         Intent intent = new Intent(this, FitnessActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoGraphics() {
+        Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
 
