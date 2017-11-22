c92ce4036eaaf59a8f427697f8313d4c10b30b57
Jason Klein <kleinjd@uwm.edu>
2017-11-22 12:50:41 -0600

    Update HomeScreen.java
    Added Context Import

--- app/src/main/java/edu/uwm/cs/fitrpg/view/HomeScreen.java
+++ app/src/main/java/edu/uwm/cs/fitrpg/view/HomeScreen.java
@@ -2,6 +2,7 @@ package edu.uwm.cs.fitrpg.view;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
